ce51073027965c723f72a0eb205ceae1f291d3c8
equimkt-development <dev@equimkt.com>
2024-09-02 18:03:02 -0400

    i tried

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -3,6 +3,27 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
+const getMarkerColor = (parcel, featureToggles) => {
+    const scores = [];
+
+    // Collect scores based on the toggled features
+    if (featureToggles.landValue) scores.push(parcel.LANDVAL);
+    if (featureToggles.gisAcres) scores.push(parcel.GISACRES);
+    if (featureToggles.valuePerAcre) scores.push(parcel.VALUE_PER_ACRE);
+    if (featureToggles.proximityScore) scores.push(parcel.PROXIMITY_SCORE);
+    if (featureToggles.trafficScore) scores.push(parcel.TRAFFIC_SCORE);
+    if (featureToggles.landValueScore) scores.push(parcel.LAND_VALUE_SCORE);
+    if (featureToggles.combinedScore) scores.push(parcel.COMBINED_SCORE);
+
+    // Determine the best score
+    const bestScore = Math.max(...scores);
+
+    // Set marker color based on the best score
+    if (bestScore < 100000) return 'red'; // Low score
+    if (bestScore < 500000) return 'yellow'; // Medium score
+    return 'green'; // High score
+};
+
 const MapComponent = ({ parcels, featureToggles }) => {
     return (
         <MapContainer center={[35.31836, -80.8322]} zoom={13} style={{ height: '100vh', width: '100%' }}>
@@ -11,10 +32,12 @@ const MapComponent = ({ parcels, featureToggles }) => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
             {parcels.map((parcel, index) => {
+                const markerColor = getMarkerColor(parcel, featureToggles); // Use the updated function
+
                 const customIcon = L.divIcon({
                     className: 'custom-marker',
                     html: `<div style="width: 25px; height: 41px; display: flex; justify-content: center; align-items: center;">
-                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" style="fill: black;">
+                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" style="fill: ${markerColor};">
                                    <path d="M50 0C33.432 0 20 13.432 20 30c0 20 30 70 30 70s30-50 30-70c0-16.568-13.432-30-30-30zm0 45c-8.284 0-15-6.716-15-15s6.716-15 15-15 15 6.716 15 15-6.716 15-15 15z"/>
                                </svg>
                            </div>`,
