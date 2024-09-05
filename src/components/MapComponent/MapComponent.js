573be411410db4c6133063235ebfee18a423c1e7
equimkt-development <dev@equimkt.com>
2024-09-05 14:14:10 -0400

    replaced heat map and markers with dots

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -2,7 +2,6 @@ import React, { useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import 'leaflet.heat';
 
 const HeatmapLayer = ({ parcels }) => {
     const map = useMap();
@@ -13,17 +12,7 @@ const HeatmapLayer = ({ parcels }) => {
             map.removeLayer(heatLayerRef.current);
         }
 
-        const heatLayer = L.heatLayer(parcels.map(parcel => [parcel.lat, parcel.lon, parcel.LANDVAL]), {
-            radius: 25,
-            blur: 15,
-            maxZoom: 17,
-        }).addTo(map);
-
-        heatLayerRef.current = heatLayer;
-
-        return () => {
-            map.removeLayer(heatLayer);
-        };
+        return () => {};
     }, [parcels, map]);
 
     return null;
@@ -42,36 +31,22 @@ const MapComponent = ({ parcels, featureToggles }) => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <HeatmapLayer parcels={parcels} />
-            {zoomLevel > 12 && parcels.map((parcel, index) => {
-                const customIcon = L.divIcon({
-                    className: 'custom-marker',
-                    html: `<div style="width: 25px; height: 41px; display: flex; justify-content: center; align-items: center;">
-                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-                                   <path d="M50 0C33.432 0 20 13.432 20 30c0 20 30 70 30 70s30-50 30-70c0-16.568-13.432-30-30-30zm0 45c-8.284 0-15-6.716-15-15s6.716-15 15-15 15 6.716 15 15-6.716 15-15 15z" fill="green" stroke="black" stroke-width="1"/>
-                               </svg>
-                           </div>`,
-                    iconSize: [25, 41],
-                    iconAnchor: [12, 41],
-                });
-
-                return (
-                    <Marker key={index} position={[parcel.lat, parcel.lon]} icon={customIcon}>
-                        <Popup>
-                            <div>
-                                <h3>{parcel.PARUSEDESC}</h3>
-                                {featureToggles.landValue && <p>Land Value: {parcel.LANDVAL}</p>}
-                                {featureToggles.gisAcres && <p>GIS Acres: {parcel.GISACRES}</p>}
-                                {featureToggles.valuePerAcre && <p>Value Per Acre: {parcel.VALUE_PER_ACRE}</p>}
-                                {featureToggles.proximityScore && <p>Proximity Score: {parcel.PROXIMITY_SCORE}</p>}
-                                {featureToggles.trafficScore && <p>Traffic Score: {parcel.TRAFFIC_SCORE}</p>}
-                                {featureToggles.landValueScore && <p>Land Value Score: {parcel.LAND_VALUE_SCORE}</p>}
-                                {featureToggles.combinedScore && <p>Combined Score: {parcel.COMBINED_SCORE}</p>}
-                            </div>
-                        </Popup>
-                    </Marker>
-                );
-            })}
+            {zoomLevel > 12 && parcels.map((parcel, index) => (
+                <Marker key={index} position={[parcel.lat, parcel.lon]} icon={L.divIcon({ className: 'simple-dot', html: '<div style="width: 10px; height: 10px; background-color: red; border-radius: 50%;"></div>', iconSize: [10, 10] })}>
+                    <Popup>
+                        <div>
+                            <h3>{parcel.PARUSEDESC}</h3>
+                            {featureToggles.landValue && <p>Land Value: {parcel.LANDVAL}</p>}
+                            {featureToggles.gisAcres && <p>GIS Acres: {parcel.GISACRES}</p>}
+                            {featureToggles.valuePerAcre && <p>Value Per Acre: {parcel.VALUE_PER_ACRE}</p>}
+                            {featureToggles.proximityScore && <p>Proximity Score: {parcel.PROXIMITY_SCORE}</p>}
+                            {featureToggles.trafficScore && <p>Traffic Score: {parcel.TRAFFIC_SCORE}</p>}
+                            {featureToggles.landValueScore && <p>Land Value Score: {parcel.LAND_VALUE_SCORE}</p>}
+                            {featureToggles.combinedScore && <p>Combined Score: {parcel.COMBINED_SCORE}</p>}
+                        </div>
+                    </Popup>
+                </Marker>
+            ))}
         </MapContainer>
     );
 };
